d1c01efeccd6c7d8cd0e5117decf49c872d6c8a0
Fiz2004 <86916308+Fiz2004@users.noreply.github.com>
2022-02-21 22:10:25 +0700

    add adapter and genres

---
+++ app/src/main/java/com/fiz/testsequenia/vp/movies/MoviesAdapter.kt
@@ -0,0 +1,99 @@
+package com.fiz.testsequenia.vp.movies
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fiz.testsequenia.databinding.ListItemGenreBinding
+import com.fiz.testsequenia.databinding.ListItemHeaderBinding
+import com.fiz.testsequenia.databinding.ListItemMovieBinding
+import com.fiz.testsequenia.model.network.models.MovieProperty
+
+private val ITEM_VIEW_TYPE_HEADER = 0
+private val ITEM_VIEW_TYPE_GENRE = 1
+private val ITEM_VIEW_TYPE_MOVIE = 2
+
+class MoviesAdapter(
+    private val genre: List<String>,
+    private val movies: List<MovieProperty>,
+    val callback: (Int) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    lateinit var data: List<DataItem>
+
+    fun addHeaderAndSubmitList() {
+        data =
+            listOf(DataItem.Header("Жанры")) + genre.map { DataItem.GenreItem(it) } + listOf(DataItem.Header("Фильмы")) + movies.map {
+                DataItem.MoviePropertyItem(it)
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> HeaderViewHolder(ListItemHeaderBinding.inflate(inflater, parent, false))
+            ITEM_VIEW_TYPE_GENRE -> GenreViewHolder(ListItemGenreBinding.inflate(inflater, parent, false))
+            ITEM_VIEW_TYPE_MOVIE -> MovieViewHolder(ListItemMovieBinding.inflate(inflater, parent, false))
+            else -> throw ClassCastException("Unknown viewType ${viewType}")
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0, genre.size + 1 -> ITEM_VIEW_TYPE_HEADER
+            in 1..genre.size -> ITEM_VIEW_TYPE_GENRE
+            else -> ITEM_VIEW_TYPE_MOVIE
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> {
+                holder.bind((data[position] as DataItem.Header).title)
+            }
+            is GenreViewHolder -> {
+                holder.bind((data[position] as DataItem.GenreItem).genre)
+            }
+            is MovieViewHolder -> {
+                holder.bind((data[position] as DataItem.MoviePropertyItem).movieProperty)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class HeaderViewHolder(val binding: ListItemHeaderBinding) : RecyclerView.ViewHolder(binding.root) {
+        var item: String? = null
+
+        fun bind(item: String) {
+            this.item = item
+            binding.text.text = item
+        }
+    }
+
+    class GenreViewHolder(val binding: ListItemGenreBinding) : RecyclerView.ViewHolder(binding.root) {
+        var item: String? = null
+
+        fun bind(item: String) {
+            this.item = item
+            binding.genreButton.text = item
+        }
+
+    }
+
+    class MovieViewHolder(val binding: ListItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        var item: MovieProperty? = null
+
+        fun bind(item: MovieProperty) {
+            this.item = item
+            binding.nameMovie.text = item.name
+        }
+    }
+}
+
+sealed class DataItem {
+    data class GenreItem(val genre: String) : DataItem()
+    data class MoviePropertyItem(val movieProperty: MovieProperty) : DataItem()
+    data class Header(val title: String) : DataItem()
+}
--- app/src/main/java/com/fiz/testsequenia/vp/movies/MoviesFragment.kt
+++ app/src/main/java/com/fiz/testsequenia/vp/movies/MoviesFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.fiz.testsequenia.databinding.FragmentMoviesBinding
+import com.fiz.testsequenia.model.network.models.MovieProperty
 import com.fiz.testsequenia.model.network.models.MoviesProperty
+import java.util.concurrent.Executors
 
 class MoviesFragment : Fragment(), IMoviesView {
     private var _binding: FragmentMoviesBinding? = null
     private val binding get() = _binding!!
 
     private var moviesPresenter: MoviesPresenter? = null
+    private lateinit var adapter: MoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,17 +25,48 @@ class MoviesFragment : Fragment(), IMoviesView {
     ): View {
         _binding = FragmentMoviesBinding.inflate(inflater, container, false)
 
+        initUI(null)
 
-        binding.playButton.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(
-                    MoviesFragmentDirections.actionMoviesFragmentToMovieDetailsFragment("1")
-                )
-        }
+//        binding.playButton.setOnClickListener { view: View ->
+//            view.findNavController()
+//                .navigate(
+//                    MoviesFragmentDirections.actionMoviesFragmentToMovieDetailsFragment("1")
+//                )
+//        }
+
 
         moviesPresenter = MoviesPresenter(this)
 
         return binding.root
+    }
+
+    private fun initUI(listResult: MoviesProperty?) {
+        if (listResult == null) return
+
+        val allGenres:MutableSet<String> = mutableSetOf()
+
+        listResult.films.map{ movie -> movie.genres.forEach { allGenres.add(it) }}
+
+        val movies = listResult.films
+
+        adapter = MoviesAdapter(allGenres.distinct(),movies) { position: Int ->
+            Toast.makeText(context, "${id}", Toast.LENGTH_LONG).show()
+//            sleepTrackerViewModel.onSleepNightClicked(nightId)
+        }
+        adapter.addHeaderAndSubmitList()
+
+        val manager = GridLayoutManager(activity, 2)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0, allGenres.size + 1 -> 2
+                in 1..allGenres.size -> 2
+                else -> 1
+            }
+        }
+            binding.moviesRecyclerView.post {
+                binding.moviesRecyclerView.layoutManager = manager
+                binding.moviesRecyclerView.adapter = adapter
+            }
     }
 
     override fun onDestroyView() {
@@ -43,6 +78,6 @@ class MoviesFragment : Fragment(), IMoviesView {
     }
 
     override fun showMovies(listResult: MoviesProperty) {
-        listResult
+        initUI(listResult)
     }
 }
