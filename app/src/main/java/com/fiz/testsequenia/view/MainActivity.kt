47a733d410fccbe469fd84eade99e64f44adef5d
Fiz2004 <86916308+Fiz2004@users.noreply.github.com>
2022-02-21 18:41:57 +0700

    Init commit

---
+++ app/src/main/java/com/fiz/testsequenia/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fiz.testsequenia.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fiz.testsequenia.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
